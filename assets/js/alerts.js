3382c92d98ae227dd5c443458547d24fef9cf922
fraydejesus <81780537+fraydejesus@users.noreply.github.com>
2023-03-14 16:39:24 -0400

    Update

---
+++ assets/js/alerts.js
@@ -0,0 +1,18 @@
+
+
+
+// alert('Hola Mundo');
+
+// let nombre = prompt('Cual es tu nombre?', 'Fray De Jesus');
+
+
+// document.write(nombre);
+// let numero1 = prompt('Primer numero');
+// let numero2 = prompt('Segundo numero');
+
+// let suma = (parseInt(numero1) + parseInt(numero2));
+// alert('Resultado de la suma es : ' + suma);
+
+const seleccion = confirm('Esta seguro de borrar esto?');
+console.log(seleccion);
+document.write(seleccion);
---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+alert('HOLA DESDE APP.JS');
+
+// PARA IMPRIMIR
+// console.log('Hola Mundo');
+// document.write('Hola Fray De Jesus');
+
+//------------------------------------------------------//
+// VARIABLES
+// var a = 20; // VARIABLE
+// let b = 30; // VARIABLE
+// const c = 50; // CONSTANTE
+
+// VARIABLES ASIGNADAS Y COMO SE PUEDEN ASIGNAR
+// let a = 15;
+// let b = 15;
+// let x = a + b;
+
+// let a = 10,
+// b = 10,
+// x = a + b;
+
+//------------------------------------------------------//
+
+// SE PUEDEN IMPRIMIR ALARMAS INTENCIONALES EN CONSOLA 
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+
+// PARA IMPRIMIR EL ASIGNADO DEL VALOR QUE SE ESTA IMPRIMIENTO
+// console.log({ a });
+// console.log({ b });
+// console.log({ x });
+
+// IMPRIME EN CONSOLA EN UNA TABLA
+// console.table({ a, b, x });
+
+//------------------------------------------------------//
+
+// PROMPT, CONFIRM y ALERT
+
+
