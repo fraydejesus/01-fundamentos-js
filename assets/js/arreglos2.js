1851109029ce978315e3e07cb0f3cd8355ca7e8e
fraydejesus <81780537+fraydejesus@users.noreply.github.com>
2023-03-17 08:29:01 -0400

    Buscar en un arreglo

--- assets/js/arreglos2.js
+++ assets/js/arreglos2.js
@@ -18,3 +18,21 @@ console.log({ nuevoJuego, juegos });
 // (unshift) PARA AGREGAR UN ELEMENTO AL PRINCIPIO DEL ARREGLO
 nuevoJuego = juegos.unshift('Lufia');
 console.log({ nuevoJuego, juegos });
+
+
+// (pop) PARA BORRA EL ULTIMO ELEMENTO DEL ARREGLO
+let juegosBorrados = juegos.pop();
+console.log({ juegosBorrados, juegos });
+
+
+
+let pos = 1; // EN ESTA VARIABLE DECLARO DE DONDE EMPIEZA 
+
+// (splice) REMUEBE UN ELEMENTO DE UN ARREGLO ASIGNAN EL NUMERO
+let juegosBorrado = juegos.splice(pos, 2); // EL 2 REPRESENTA LA CANTIDAD DE ELEMENTOS A BORRA EN EL ARREGLO
+console.log({ juegosBorrado, juegos });
+
+// (indexOf) SE UTILIZA PARA BUSCAR ALGO DENTRO DEL ARREGLO
+let buscarEnArreglo = juegos.indexOf('Metroid');
+console.log({ buscarEnArreglo });
+
