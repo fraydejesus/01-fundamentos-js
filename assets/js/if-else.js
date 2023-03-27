533ed6243254b50220ffeb873ed84b31cc0f1438
fraydejesus <81780537+fraydejesus@users.noreply.github.com>
2023-03-27 11:16:24 -0400

    booleana

---
+++ assets/js/if-else.js
@@ -0,0 +1,36 @@
+
+
+// let a = 5;
+
+// if (a > 10) {
+//     console.log('A es mayor a 10');
+// }
+// else {
+//     console.log('A es menor a 10');
+// }
+
+// //console.log('Fin del programa');
+
+// const hoy = new Date(); // TRAE INFORMACION CON RESPECTO AL DIA
+// let dia2 = hoy.getDay(); // 0: DOMINGO, 1:LUNES, 2:MARTES, 3:MIERCOLES, 4:JUEVES, 5:VIERNES, 6:SABADO
+
+// console.log(dia);
+
+
+// CON OBJETO PARA SACAR LOS DIAS DE LA SEMANA
+dia = 5;
+
+const diasLetras = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado',
+}
+
+//CON ARREGLO
+const diasLetras2 = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado',];
+
+console.log(diasLetras2[dia]);
