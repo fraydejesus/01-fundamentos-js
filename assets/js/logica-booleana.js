bcc5d260756da9ee56258913b0a7d997f8946818
fraydejesus <81780537+fraydejesus@users.noreply.github.com>
2023-03-28 16:03:03 -0400

    update

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -1,4 +1,4 @@
-
+// AND (&&)
 
 const regresaTrue = () => {
     console.log('Regresa true');
@@ -16,3 +16,23 @@ console.warn('Not o la negacion');
 console.log(true);
 console.log(!true); // (!) CAMBIA EL RESULTADO SI ES (true lo cambia a false y lo contrario)
 console.log(!false);
+
+console.log(!regresaFalse());
+
+console.warn('and'); // REGRESA true SI TODOS LOS VALORES SON VERDADEROS 
+console.log(true && true);
+console.log(true && !false);
+
+console.log(regresaFalse() && regresaTrue()); //CUAND LA PREIMERA ES false ya no evalua POR QUE EL RESULTADO ES FALSO
+console.log(regresaTrue() && regresaFalse()); //EVALUA TODO YA QUE LA PRIMERA ES VERDADERA
+
+regresaTrue() && regresaFalse()  // ES PERMITIDO 
+
+
+// OR (||) REGRESA VERDADERO SIEMPRE Y CUANDO AYA 1 VALOR VERDADERO
+
+console.warn('or');
+console.log(true || true);
+console.log(!true || false);
+
+
